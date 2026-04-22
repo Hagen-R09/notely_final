eab15bb0097ca3e31593b0831bcc3b599cc59090
Hagen-R09 <s1014561@student.knoxschools.org>
2026-04-22 18:02:12 +0000

    database worked on

---
+++ seed_db.py
@@ -0,0 +1,13 @@
+from database import get_db, init_db
+import bcrypt
+
+def seed_database():
+        """add sample users to the database"""
+        init_db()
+
+        conn = get_db
+
+    
+        sample_entries = [
+            ('interesting', 'today was interesting')
+        ]
