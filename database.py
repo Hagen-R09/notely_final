b96507f17c560fe3d22b2fab7ea50ea6e994e50a
Hagen-R09 <s1014561@student.knoxschools.org>
2026-05-15 14:47:18 +0000

    yup

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ def init_db():
     conn.execute("""
         CREATE TABLE IF NOT EXISTS pieces (
             id INTEGER PRIMARY KEY AUTOINCREMENT
-            piecename TEXT KEY,
+            piecename TEXT,
             work TEXT
         )
     """)
